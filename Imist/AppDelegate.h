cd3c15693e73a5424b1309b1b8cbf913e593d71e
chao.ou <bobo@nickolematoMacBook-Pro.local>
2014-12-27 17:54:23 +0800

    Initial Commit

---
+++ Imist/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Imist
+//
+//  Created by chao.ou on 14/12/27.
+//  Copyright (c) 2014年 oc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Imist/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Imist
+//
+//  Created by chao.ou on 14/12/27.
+//  Copyright (c) 2014年 oc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
